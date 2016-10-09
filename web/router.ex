373c3cf19939743be5afbf9bd68b75a0af5a3847
peterpih <railsnewbie257@gmail.com>
2016-10-09 17:35:12 -0500

    hello_phoenix

--- web/router.ex
+++ web/router.ex
@@ -17,7 +17,10 @@ defmodule HelloPhoenix.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/hello", HelloController, :index
   end
+
+
 
   # Other scopes may use custom stacks.
   # scope "/api", HelloPhoenix do
